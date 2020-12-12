f146336cd03dfa91079264bfa6a03803ca1deb2d
Sergey Ziryanov <redapple54r@gmail.com>
2020-12-13 00:56:29 +0700

    Orders model and migration

---
+++ src/database/migrations/2020_12_12_174916_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('store_item_id')->unsigned()->nullable();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('currency_id')->unsigned();
+            $table->integer('count')->nullable();
+            $table->timestamps();
+
+            $table->foreign('store_item_id')->references('id')->on('store_items');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('currency_id')->references('id')->on('currencies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
